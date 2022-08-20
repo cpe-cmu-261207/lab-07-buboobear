963370837cbae06b78e93605bf79a2a8758ef754
bobubear <jedsa.pd@gmail.com>
2022-08-20 13:33:58 +0700

    finish:)

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,117 @@
 import { useEffect, useState } from "react";
 import Todo from "../components/Todo";
 import {
-  IconCheck,
-  IconTrash,
-  IconArrowUp,
-  IconArrowDown,
+    IconCheck,
+    IconTrash,
+    IconArrowUp,
+    IconArrowDown,
 } from "@tabler/icons";
 
 export default function Home() {
-  const deleteTodo = (idx) => {};
+    const [todo, setTodo] = useState([]);
 
-  const markTodo = (idx) => {};
+    const [isFirstRender, setIsFirstRender] = useState(true);
+    useEffect(() => {
+        if (isFirstRender) {
+            setIsFirstRender(false);
+            return;
+        }
+        const todoStr = JSON.stringify(todo);
+        localStorage.setItem("todo-react", todoStr);
+    }, [todo]);
 
-  const moveUp = (idx) => {};
+    useEffect(() => {
+        const todoStr = localStorage.getItem("todo-react");
+        if (!todoStr) setTodo([]);
+        else setTodo(JSON.parse(todoStr));
+    }, []);
 
-  const moveDown = (idx) => {};
+    const deleteTodo = (idx) => {
+        todo.splice(idx, 1);
+        setTodo([...todo]);
+    };
 
-  return (
-    <div>
-      {/* Entire App container (required for centering) */}
-      <div style={{ maxWidth: "700px" }} className="mx-auto">
-        {/* App header */}
-        <p className="display-4 text-center fst-italic m-4">
-          Minimal Todo List <span className="fst-normal">☑️</span>
-        </p>
-        {/* Input */}
-        <input
-          className="form-control mb-1 fs-4"
-          placeholder="insert todo here..."
-        />
-        {/* Todos */}
-        {/* Example 1 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo</span>
+    const markTodo = (idx) => {
+        todo[idx].completed = !todo[idx].completed;
+        setTodo([...todo]);
+    };
+
+    const moveUp = (idx) => {
+        let temp = todo[idx];
+        todo[idx] = todo[idx - 1];
+        todo[idx - 1] = temp;
+        setTodo([...todo]);
+    };
+
+    const moveDown = (idx) => {
+        if (idx == todo.length - 1) {
+            return;
+        }
+        let temp = todo[idx];
+        todo[idx] = todo[idx + 1];
+        todo[idx + 1] = temp;
+        setTodo([...todo]);
+    };
+
+    const addTodo = (title, completed) => {
+        setTodo([{ title: title, completed: completed }, ...todo]);
+    };
+
+    const handleInput = (event) => {
+        if (event.key == "Enter") {
+            if (event.target.value === "") {
+                alert("Todo cannot be empty");
+                return;
+            }
+            addTodo(event.target.value, false);
+            event.target.value = "";
+        }
+    };
+
+    return (
+        <div>
+            {/* Entire App container (required for centering) */}
+            <div style={{ maxWidth: "700px" }} className="mx-auto">
+                {/* App header */}
+                <p className="display-4 text-center fst-italic m-4">
+                    Minimal Todo List <span className="fst-normal">☑️</span>
+                </p>
+                {/* Input */}
+                <input
+                    className="form-control mb-1 fs-4"
+                    placeholder="insert todo here..."
+                    onKeyUp={handleInput}
+                />
+                {/* Todos */}
+                {todo.map((element, index) => (
+                    <Todo
+                        key={index}
+                        title={element.title}
+                        completed={element.completed}
+                        onMark={() => markTodo(index)}
+                        onDelete={() => deleteTodo(index)}
+                        onMoveUp={() => moveUp(index)}
+                        onMoveDown={() => moveDown(index)}
+                    />
+                ))}
+                {/* summary section */}
+                <p className="text-center fs-4">
+                    <span className="text-primary">All ({todo.length}) </span>
+                    <span className="text-warning">
+                        Pending (
+                        {todo.filter((e) => e.completed == false).length})
+                    </span>
+                    <span className="text-success">
+                        Completed (
+                        {todo.filter((e) => e.completed == true).length})
+                    </span>
+                </p>
+
+                {/* Made by section */}
+                <p className="text-center mt-3 text-muted fst-italic">
+                    made by Jedsadaporn Juntong 640612179
+                </p>
+            </div>
         </div>
-        {/* Example 2 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo with buttons</span>
-
-          <button className="btn btn-success">
-            <IconCheck />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowUp />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowDown />
-          </button>
-          <button className="btn btn-danger">
-            <IconTrash />
-          </button>
-        </div>
-
-        {/* summary section */}
-        <p className="text-center fs-4">
-          <span className="text-primary">All (2) </span>
-          <span className="text-warning">Pending (2) </span>
-          <span className="text-success">Completed (0)</span>
-        </p>
-
-        {/* Made by section */}
-        <p className="text-center mt-3 text-muted fst-italic">
-          made by Chayanin Suatap 12345679
-        </p>
-      </div>
-    </div>
-  );
+    );
 }
